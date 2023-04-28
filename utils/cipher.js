c74a20f6154325f3bc680bed13f3d23c7bc208dc
Mehar-It <hamzatariquearain@gmail.com>
2023-04-28 22:03:10 +0500

    fixed bug

--- utils/cipher.js
+++ utils/cipher.js
@@ -1,27 +1,27 @@
 // encryption.js
 const crypto = require("crypto");
 
-const { secret_key, secret_iv, ecnryption_method } = process.env;
+const { SECRET_KEY, SECRET_IV, ECNRYPTION_METHOD } = process.env;
 
-if (!secret_key || !secret_iv || !ecnryption_method) {
+if (!SECRET_KEY || !SECRET_IV || !ECNRYPTION_METHOD) {
   throw new Error("secretKey, secretIV, and ecnryptionMethod are required");
 }
 
 // Generate secret hash with crypto to use for encryption
 const key = crypto
   .createHash("sha512")
-  .update(secret_key)
+  .update(SECRET_KEY)
   .digest("hex")
   .substring(0, 32);
 const encryptionIV = crypto
   .createHash("sha512")
-  .update(secret_iv)
+  .update(SECRET_IV)
   .digest("hex")
   .substring(0, 16);
 
 // Encrypt data
 exports.encryptData = (data) => {
-  const cipher = crypto.createCipheriv(ecnryption_method, key, encryptionIV);
+  const cipher = crypto.createCipheriv(ECNRYPTION_METHOD, key, encryptionIV);
   return Buffer.from(
     cipher.update(data, "utf8", "hex") + cipher.final("hex")
   ).toString("base64"); // Encrypts data and converts to hex and base64
@@ -31,7 +31,7 @@ exports.encryptData = (data) => {
 exports.decryptData = (encryptedData) => {
   const buff = Buffer.from(encryptedData, "base64");
   const decipher = crypto.createDecipheriv(
-    ecnryption_method,
+    ECNRYPTION_METHOD,
     key,
     encryptionIV
   );
