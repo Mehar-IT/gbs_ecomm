1c84b0ae82045b8bd5459b887cd2df86c70fc64c
Mehar-It <hamzatariquearain@gmail.com>
2023-05-01 14:57:20 +0500

    added product type

--- models/productModel.js
+++ models/productModel.js
@@ -20,6 +20,10 @@ const productSchema = new mongoose.Schema(
     //   type: Number,
     //   default: 0,
     // },
+    productType: {
+      type: String,
+      require: true,
+    },
     images: [
       {
         public_id: {
