0570a9d6d93c74c167f2e27c1a434abbe7526421
Mehar-It <hamzatariquearain@gmail.com>
2023-05-03 12:06:33 +0500

    added firstname and lastname feild in orders model

--- models/orderModel.js
+++ models/orderModel.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose");
 const orderSchema = new mongoose.Schema(
   {
     shippingInfo: {
+      firstName: { type: String, required: true },
+      lastName: { type: String, required: true },
       address: { type: String, required: true },
       city: { type: String, required: true },
       state: { type: String, required: true },
