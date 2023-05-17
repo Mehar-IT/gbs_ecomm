f80cc19a5351c7e0d324d2036c39183154307fa6
Mehar-It <hamzatariquearain@gmail.com>
2023-05-17 13:29:38 +0500

    fixed bugs

--- models/accountModel.js
+++ models/accountModel.js
@@ -2,11 +2,6 @@ const mongoose = require("mongoose");
 
 const accountSchema = new mongoose.Schema(
   {
-    category: {
-      type: String,
-      unique: true,
-      trim: true,
-    },
     accTitle: {
       type: String,
       required: true,
