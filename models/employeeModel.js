2a82f8f69f274a285ade7a19a00565fadef33937
Mehar-It <hamzatariquearain@gmail.com>
2023-05-18 15:15:48 +0500

    fixed yarn.lock file

--- models/employeeModel.js
+++ models/employeeModel.js
@@ -3,47 +3,48 @@ const validator = require("validator");
 const JWT = require("jsonwebtoken");
 const { encryptData } = require("../utils/cipher");
 
-const employeeSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "please enter your name"],
-    maxLength: [30, "name can not exceed 30 character "],
-    minLength: [4, "name should have more than 4 character"],
-  },
-  email: {
-    type: String,
-    required: [true, "please enter your email"],
-    unique: true,
-    validate: [validator.isEmail, "please enter a valid email"],
-  },
-  password: {
-    type: String,
-    required: [true, "please enter your password"],
-    minLength: [8, "password should be greater than 8 characters"],
-  },
-  avatar: {
-    public_id: {
+const employeeSchema = new mongoose.Schema(
+  {
+    name: {
       type: String,
-      required: true,
+      required: [true, "please enter your name"],
+      maxLength: [30, "name can not exceed 30 character "],
+      minLength: [4, "name should have more than 4 character"],
     },
-    url: {
+    email: {
       type: String,
-      required: true,
+      required: [true, "please enter your email"],
+      unique: true,
+      validate: [validator.isEmail, "please enter a valid email"],
+    },
+    password: {
+      type: String,
+      required: [true, "please enter your password"],
+      minLength: [8, "password should be greater than 8 characters"],
+    },
+    avatar: {
+      public_id: {
+        type: String,
+        required: true,
+      },
+      url: {
+        type: String,
+        required: true,
+      },
+    },
+    role: {
+      type: String,
+      required: [true, "Role is required"],
+    },
+    approvalByAdmin: {
+      type: Boolean,
+      default: true,
     },
   },
-  role: {
-    type: String,
-    required: [true, "Role is required"],
-  },
-  approvalByAdmin: {
-    type: Boolean,
-    default: true,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 employeeSchema.methods.getJwtToken = function () {
   const token = JWT.sign({ id: this._id }, process.env.JWT_KEY, {
